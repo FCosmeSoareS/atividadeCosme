c8286b76f790d63b98f4e74069ce940cd5db8051
robinsonmourao <bob.info.guaratiba@gmail.com>
2022-11-18 18:08:54 -0300

    imprime nome com branch

--- projetoCosme/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module projetoCosme {
-	requires java.desktop;
-}
---
+++ projetoCosme/src/projetoCosmeSoares/ImprimeNome2.java
@@ -0,0 +1,10 @@
+package projetoCosmeSoares;
+
+public class ImprimeNome2 {
+
+	public static void main(String[] args) {
+		System.out.println("Robinson2");
+
+	}
+
+}
